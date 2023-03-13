5f4b1fae7a6970bda65d49f3eac1368af74085c9
luiz <lguilhermefl@gmail.com>
2023-03-13 18:58:33 -0300

    feat: add panel nav

---
+++ src/components/Panel/PanelNav.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="btn-group">
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+* {
+  --var-bs-border-width: 1px;
+}
+
+.btn-group {
+  display: flex;
+  justify-content: center;
+  width: fit-content;
+  margin-bottom: 2rem;
+}
+
+.btn-group > :not(.btn:first-child) {
+  margin-left: calc(var(--var-bs-border-width) * -1);
+}
+</style>
