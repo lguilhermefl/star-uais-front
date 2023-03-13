6c946d48393e99b7319f7834a18bd0745534a44f
luiz <lguilhermefl@gmail.com>
2023-03-13 18:57:34 -0300

    feat: add panel config

---
+++ src/components/Panel/config.ts
@@ -0,0 +1,19 @@
+import type { Collection } from './types'
+
+export const collectionsToFetch: Collection[] = [
+  'Films',
+  'People',
+  'Planets',
+  'Species',
+  'Starships',
+  'Vehicles'
+]
+
+export const collectionsNames: string[] = [
+  'Filmes',
+  'Pessoas',
+  'Planetas',
+  'Espécies',
+  'Naves',
+  'Veículos'
+]
