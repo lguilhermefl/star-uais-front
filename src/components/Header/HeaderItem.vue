c41d83b20542454a4330219d1f8ee994fa390f85
luiz <lguilhermefl@gmail.com>
2023-03-12 22:21:45 -0300

    feat: add header

---
+++ src/components/Header/HeaderItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <img alt="Star Uais logo" class="logo" src="@/assets/logo.svg" />
+    <div>
+      <h1 class="heading">Pensa num trem bão cheio de informações para os fãs de Star Wars!</h1>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+  margin-bottom: 2rem;
+}
+.logo {
+  max-height: 70px;
+}
+
+.heading {
+  color: var(--vt-c-white);
+  font-size: 1.5rem;
+}
+</style>
