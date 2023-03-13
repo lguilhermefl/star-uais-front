f8655adb409617af0f248b7734de714e8b674e90
luiz <lguilhermefl@gmail.com>
2023-03-13 19:07:26 -0300

    feat: add people card

---
+++ src/components/Panel/Cards/PeopleCard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import CardItem from './CardItem.vue'
+
+defineProps<{
+  item: any
+}>()
+</script>
+
+<template>
+  <CardItem>
+    <template #title>{{ item.name }}</template>
+    <template #content>
+      <span>Altura</span>: {{ item.height }} cm<br />
+      <span>Peso</span>: {{ item.mass }} kg<br />
+      <span>Cor dos cabelos</span>: {{ item.hair_color }}<br />
+      <span>Cor dos olhos</span>: {{ item.eye_color }}<br />
+      <span>Ano de nascimento</span>: {{ item.birth_year }}<br />
+    </template>
+  </CardItem>
+</template>
+
+<style scoped>
+.opening {
+  text-align: center;
+}
+</style>
