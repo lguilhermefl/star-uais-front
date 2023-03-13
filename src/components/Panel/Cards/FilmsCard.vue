0c66a4da7e120c26460b56ff43ae160d402b0897
luiz <lguilhermefl@gmail.com>
2023-03-13 19:07:11 -0300

    feat: add films card

---
+++ src/components/Panel/Cards/FilmsCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import CardItem from './CardItem.vue'
+
+defineProps<{
+  item: any
+}>()
+</script>
+
+<template>
+  <CardItem>
+    <template #title>{{ item.title }}</template>
+    <template #content>
+      <span>Produtor</span>: {{ item.producer }}<br />
+      <span>Diretor</span>: {{ item.director }}<br />
+      <span>Data de lançamento</span>: {{ item.release_date }}<br />
+      <span>Texto de abertura</span>: {{ item.opening_crawl }}<br />
+    </template>
+  </CardItem>
+</template>
