230d05a21bf2a1135176515f660abf850693bb1c
luiz <lguilhermefl@gmail.com>
2023-03-12 22:28:07 -0300

    feat: add auth form

---
+++ src/components/InfoAndAuth/AuthForm.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import SignInForm from './AuthFormItems/SignInForm.vue'
+import SignUpForm from './AuthFormItems/SignUpForm.vue'
+import { ref } from 'vue'
+
+const showSignIn = ref(true)
+
+const showSignInControler = (newValue: boolean): void => {
+  showSignIn.value = newValue
+}
+</script>
+
+<template>
+  <Transition name="fade" mode="out-in">
+    <SignInForm v-if="showSignIn" @show-sign-in="showSignInControler" />
+    <SignUpForm v-else @show-sign-in="showSignInControler" />
+  </Transition>
+</template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
