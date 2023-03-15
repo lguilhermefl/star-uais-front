338088027e10672aac49f6aadc4823b5aab7a84a
luiz <lguilhermefl@gmail.com>
2023-03-14 21:30:03 -0300

    feat: add account icon component

---
+++ src/components/Header/AccountIcon.vue
@@ -0,0 +1,14 @@
+<template>
+  <img src="@/assets/accountIcon.svg" alt="Account Icon" class="account-icon" />
+</template>
+
+<style>
+.account-icon {
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  text-align: center;
+  line-height: 35px;
+  vertical-align: middle;
+}
+</style>
