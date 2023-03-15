f52557d49a15aebdafcec63ed8e4cff5579b6523
luiz <lguilhermefl@gmail.com>
2023-03-14 21:12:04 -0300

    fix: abreviate to avoid line break

--- src/components/Home/Cards/StarshipsCard.vue
+++ src/components/Home/Cards/StarshipsCard.vue
@@ -12,14 +12,14 @@ defineProps<{
     <template #content>
       <span>Velocidade máxima</span>: {{ item.MGLT }}/h<br />
       <span>Carga máxima</span>: {{ item.cargo_capacity }} kg<br />
-      <span>Tempo de suprimento para tripulação</span>: {{ item.consumables }}<br />
+      <span>Tempo de suprimento</span>: {{ item.consumables }}<br />
       <span>Preço</span>: {{ item.cost_in_credits }} créditos<br />
-      <span>Pessoal necessário p/ pilotar</span>: {{ item.crew }}<br />
+      <span>Pessoas p/ pilotar</span>: {{ item.crew }}<br />
       <span>Passageiros</span>: {{ item.passengers }}<br />
       <span>Classe hyperdrive</span>: {{ item.hyperdrive_rating }}<br />
       <span>Comprimento</span>: {{ item.length }} m<br />
       <span>Fabricante</span>: {{ item.manufacturer }}<br />
-      <span>Velocidade máxima na atmosfera</span>: {{ item.max_atmosphering_speed }}<br />
+      <span>Velocidade máx. na atmosfera</span>: {{ item.max_atmosphering_speed }}<br />
       <span>Modelo</span>: {{ item.model }}<br />
       <span>Classe</span>: {{ item.starship_class }}<br />
     </template>
--- src/components/Home/Cards/VehiclesCard.vue
+++ src/components/Home/Cards/VehiclesCard.vue
@@ -10,15 +10,14 @@ defineProps<{
   <CardItem>
     <template #title>{{ item.name }}</template>
     <template #content>
-      <span>Velocidade máxima</span>: {{ item.MGLT }}/h<br />
       <span>Carga máxima</span>: {{ item.cargo_capacity }} kg<br />
-      <span>Tempo de suprimento para tripulação</span>: {{ item.consumables }}<br />
+      <span>Tempo de suprimento</span>: {{ item.consumables }}<br />
       <span>Preço</span>: {{ item.cost_in_credits }} créditos<br />
-      <span>Pessoal necessário p/ pilotar</span>: {{ item.crew }}<br />
+      <span>Pessoas p/ pilotar</span>: {{ item.crew }}<br />
       <span>Passageiros</span>: {{ item.passengers }}<br />
       <span>Comprimento</span>: {{ item.length }} m<br />
       <span>Fabricante</span>: {{ item.manufacturer }}<br />
-      <span>Velocidade máxima na atmosfera</span>: {{ item.max_atmosphering_speed }}<br />
+      <span>Velocidade máx. na atmosfera</span>: {{ item.max_atmosphering_speed }}<br />
       <span>Modelo</span>: {{ item.model }}<br />
       <span>Classe</span>: {{ item.vehicle_class }}<br />
     </template>
